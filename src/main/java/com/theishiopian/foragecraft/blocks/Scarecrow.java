bdbc727b0379c4b428dec9661b80f78e85949f95
Jonathan Colmenares <jonathan@yolouso.com>
2017-12-16 13:57:52 -0500

    Implemented Bailey's fix for scarecrow lighting issue

--- src/main/java/com/theishiopian/foragecraft/blocks/Scarecrow.java
+++ src/main/java/com/theishiopian/foragecraft/blocks/Scarecrow.java
@@ -38,6 +38,7 @@ public class Scarecrow extends BlockHorizontal
 		setRegistryName("scarecrow");
 		setCreativeTab(CreativeTabs.DECORATIONS);
 		setSoundType(SoundType.WOOD);
+		this.useNeighborBrightness = true;
 		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 		
 	}
