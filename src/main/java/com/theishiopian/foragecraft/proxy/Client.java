6a71cab70ef414e14504e0349791e076df06e9e3
theishiopian <theishiopian@gmail.com>
2018-01-22 15:38:18 -0800

    Fixed entity rendering, closes #51

--- src/main/java/com/theishiopian/foragecraft/proxy/Client.java
+++ src/main/java/com/theishiopian/foragecraft/proxy/Client.java
@@ -12,38 +12,18 @@ import com.theishiopian.foragecraft.render.RenderRockNormal;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public class Client extends CommonProxy
 {
-	@Override
-	public void preInit(FMLPreInitializationEvent e) 
-	{
-		RenderingRegistry.registerEntityRenderingHandler(EntityRockNormal.class, RenderRockNormal.FACTORY);
-		RenderingRegistry.registerEntityRenderingHandler(EntityRockFlat.class, RenderRockFlat.FACTORY);
-		RenderingRegistry.registerEntityRenderingHandler(EntityCrow.class, RenderCrow.FACTORY);
-    }
-	
-	@Override
-	public void init(FMLInitializationEvent e)
-	{
-		
-	}
-	
-	@Override
-	public void postInit(FMLPostInitializationEvent e) 
-    {
-       
-    }
-	
 	@SubscribeEvent
 	public static void registerModels(ModelRegistryEvent event)
 	{
 		ModBlocks.initModels();
 		ModItems.initModels();
+		RenderingRegistry.registerEntityRenderingHandler(EntityRockNormal.class, RenderRockNormal.FACTORY);
+		RenderingRegistry.registerEntityRenderingHandler(EntityRockFlat.class, RenderRockFlat.FACTORY);
+		RenderingRegistry.registerEntityRenderingHandler(EntityCrow.class, RenderCrow.FACTORY);
 	}
 }
