99462ec813874e1bcf6dd91f70290015863791a3
theishiopian <theishiopian@gmail.com>
2018-04-07 20:46:18 -0700

    Updated version number format. We are now using an x.y.z system:
    X: major changes to the underlying code, such as the ForageHandler or the main mod file
    Y: new features that dont require a major revision of existing code (new items, mobs, etc)
    Z: bugfixes
    
    Since I am making major changes to the Forage handler to allow for loot table support, I have incremented the major version to 2.

--- src/main/java/com/theishiopian/foragecraft/ForageCraftMod.java
+++ src/main/java/com/theishiopian/foragecraft/ForageCraftMod.java
@@ -40,7 +40,7 @@ public class ForageCraftMod
 	public static final String MODID = "foragecraft";
 	public static final String MOD_NAME = "ForageCraft";
 	public static final String NAME = "ForageCraft";
-	public static final String VERSION = "1.15-dev";
+	public static final String VERSION = "2.0.0-dev";
 	public static final String MC_VERSIONS = "[1.12], [1.12.1], [1.12.2]";
 	public static final String UPDATE_JSON = "https://raw.githubusercontent.com/theishiopian/ForageCraft/master/update.json";
 	public static final String PROXY_CLIENT = "com.theishiopian.foragecraft.proxy.Client";
