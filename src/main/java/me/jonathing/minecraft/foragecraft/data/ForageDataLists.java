609ea859caa5a39bddba58979f37e2486e2edb96
Jonathan Colmenares <jonathing.projects@gmail.com>
2021-01-22 22:53:05 -0500

    Attempt to fix the workflow

--- src/main/java/me/jonathing/minecraft/foragecraft/data/ForageDataLists.java
+++ src/main/java/me/jonathing/minecraft/foragecraft/data/ForageDataLists.java
@@ -1,23 +1,23 @@
 package me.jonathing.minecraft.foragecraft.data;
 
-import com.google.common.collect.ImmutableList;
 import me.jonathing.minecraft.foragecraft.ForageCraft;
 import net.minecraft.profiler.IProfiler;
 import net.minecraft.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class ForageDataLists
 {
-	/**
-	 * List of all of the Patchouli recipes to skip parsing if Patchouli is not installed.
-	 *
-	 * @see me.jonathing.minecraft.foragecraft.asm.mixin.RecipeManagerMixin#apply(Map, IResourceManager, IProfiler, CallbackInfo)
-	 */
-	public static final List<ResourceLocation> PATCHOULI_RECIPES = ImmutableList.of(
-			ForageCraft.locate("guide_book")
-	);
+    /**
+     * List of all of the Patchouli recipes to skip parsing if Patchouli is not installed.
+     *
+     * @see me.jonathing.minecraft.foragecraft.asm.mixin.RecipeManagerMixin#apply(Map, IResourceManager, IProfiler, CallbackInfo)
+     */
+    public static final List<ResourceLocation> PATCHOULI_RECIPES = Collections.singletonList(
+            ForageCraft.locate("guide_book")
+    );
 }
